9490fae9f69d86e59da8d1da7bb5a60ab26ac64a
Matt Littleson <matt.littleson@gmail.com>
2021-03-29 00:08:08 +1100

    clicking a button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const puppeteer = require('puppeteer')
 
 async function main() {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
+
   const page = await browser.newPage()
-  await page.goto('https://example.com')
-  await page.screenshot({ path: 'example.png' })
+  await page.goto('https://mattlittleson.github.io/puppeteo/')
+  await page.screenshot({ path: 'before.png' })
+
+  const button = await page.$('#button')
+  if (button) {
+    await button.click()
+  }
+
+  await page.screenshot({ path: 'after.png' })
 
   await browser.close()
 }
